2d9de4b6d696f473aa58074987c7629c4cdc4652
rizwankce <rizwan@scantrust.com>
2015-07-09 22:43:18 +0800

    Adding search API and send message with top 5 results

--- lieren_bot.js
+++ lieren_bot.js
@@ -2,7 +2,7 @@ var Bot = require('node-telegram-bot');
 var unirest = require('unirest');
 var algoliasearch = require('algoliasearch');
 
-var client = algoliasearch('applicationID', '0H4SMABBSG');
+var client = algoliasearch('0H4SMABBSG', '9670d2d619b9d07859448d7628eea5f3');
 var index = client.initIndex('Post_production');
 
 
@@ -13,14 +13,22 @@ var bot = new Bot({
   console.log(message);
   index.search(message.text, function searchDone(err, content) {
     console.log(err, content);
+    var hits = content.hits;
+    var answerText = "\n";
+    for (var i = 0; i < 5; i++) {
+      j = i+1;
+      answerText = answerText + "\n"+j+". "+ hits[i].name + " - "+hits[i].tagline +"\n"+ hits[i].url+"\n";
+    }
+    var answer = {
+        chat_id : message.chat.id,
+        disable_web_page_preview : true,
+        text : "Here you go! Top five results for "+message.text+ answerText
+    };
+      bot.sendMessage(answer,function(){
+      console.log("sucess of the india");
   });
   var caps = message.text.toUpperCase();
-  var answer = {
-      chat_id : message.chat.id,
-      text : "You told be to do something, so I took your input and made it all caps. Look: " + caps
-  };
-    bot.sendMessage(answer,function(){
-    console.log("sucess of the india");
+
   });
 })
 .start();
